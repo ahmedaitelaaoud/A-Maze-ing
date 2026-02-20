f2692525a9e7f89891a0a37c4bdb7d423ce58c32
ahmedaitelaaoud <ahmedaitelaaoud@gmail.com>
2026-02-20 03:15:42 +0000

    feat: add initial module documentation for mazegen

---
+++ src/mazegen/__init__.py
@@ -0,0 +1,10 @@
+"""
+mazegen - A reusable maze generation library.
+
+This module provides a robust, seedable maze generator using the Iterative
+Recursive Backtracker algorithm to create perfect mazes.
+"""
+
+from .generator import MazeGenerator
+
+__all__ = ["MazeGenerator"]
